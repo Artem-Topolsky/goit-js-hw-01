c989dcd703bb339dc81ed783ee83622e1787c8f4
Artem Topolsky <almost210690@gmail.com>
2025-04-01 20:21:39 +0300

    add console.log

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,6 @@
-// function getElementWidth(content, padding, border) {
-//     const contentWidth = Number.parseFloat(content); 
-//     const paddingWidth = Number.parseFloat(padding); 
-//     const borderWidth = Number.parseFloat(border);  
-//     const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-//     return totalWidth;
-// } 
-
 function getElementWidth(content, padding, border) {
     return parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
 } 
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
